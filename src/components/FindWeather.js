44a5369265d48894050da33242587b63e6a8b204
issoDev <issife@hotmail.fr>
2020-11-29 00:34:45 +0100

    add components of the app

---
+++ src/components/FindWeather.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function FindWeather() {
+  return (
+    <View>
+      <Text>Find Weather</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ src/components/SearchField.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function SearchField() {
+  return (
+    <View>
+      <Text>Search Field</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
